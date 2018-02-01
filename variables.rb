e8609373a4efe84de4333bd68fa539a716f7a2c9
Keith Turner <turner@keithturner.com>
2018-02-01 08:10:32 -0700

    Initial commit

---
+++ variables.rb
@@ -0,0 +1,8 @@
+# variables are like nicknames. With variables you give a nickname to something you use regularly.
+
+
+# x, y, banana2, phone_a_quail ar all variables
+
+teddy_bear_fee = 121.08
+
+total = orphan_fee + teddy_bear_fee + gratuity
