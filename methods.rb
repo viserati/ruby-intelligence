f55d8519341a1f0c52ead006142020283e216678
Keith Turner <turner@keithturner.com>
2018-02-04 08:18:22 -0700

    Adding data types 002

---
+++ methods.rb
@@ -0,0 +1,12 @@
+# Methods are actions, like a verb they do something. They are usually attached to the
+# end of variables and constants with a dot.
+
+front_door.open
+
+# Methods can be chained together, this is known as "Method Chaining"
+
+front_door.open.close
+
+# This is a method that tests to see if the door is open
+
+front_door.is_open?
