db8ccd444a30fbf7450e0b6aadf698666e547f72
Keith Turner <turner@keithturner.com>
2018-02-05 10:08:54 -0700

    Adding data types 004

--- methods.rb
+++ methods.rb
@@ -1,5 +1,5 @@
 # Methods are actions, like a verb they do something. They are usually attached to the
-# end of variables and constants with a dot.
+# end of variables and constants with a dot. These are instance methods.
 
 front_door.open
 
@@ -10,3 +10,13 @@ front_door.open.close
 # This is a method that tests to see if the door is open
 
 front_door.is_open?
+
+# Method Arguments are attached to the end of a method. Thehe arguments are usually
+# surrounded by parentheses and separated by commas.
+
+front_door.paint (3, :red)
+
+# Class Methods are usually attached after variables and constants. Rather than
+# a dot . they use a double colon ::
+
+Door::new (:oak)
--- variables.rb
+++ variables.rb
@@ -11,3 +11,21 @@ orphan_fee = 1000.00
 teddy_bear_fee = 100.00
 
 gratuity = 0.10
+
+# Global Variables begin with a dollar sign $ and are the same in across every
+# instance. Global variables can be used anywhere in your program. They never
+# go out of sight.
+
+$dad = Bob # The same dad named Bob in every house.
+
+# Instance Variables begin with an At symbol @. These variables are often used
+# to define the attributes of something, one thing. They are used to define the characteristics
+# of a single object in ruby. Think of the @ as attribute.
+
+front_door @width = 3 # The width of the front door is 3 feet.
+
+# Class Variables begin with double At symbols @@. These variables are used to
+# define the attributes of multiple relaed objects. All simialiar ojects have
+# the same attribute. Think of @@ as attribute all.
+
+Door @@width = 3 # Everything that is a Door has a width of 3 feet.
