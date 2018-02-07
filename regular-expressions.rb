43ad51d42df208ecb1f97d25fbef8073a3ce0fe7
Keith Turner <turner@keithturner.com>
2018-02-06 23:00:40 -0700

    Adding data types 005

---
+++ regular-expressions.rb
@@ -0,0 +1,7 @@
+# A regular expression (or regexp) is a set of characters surrounded by slashes.
+# Regular expressions are used to find words or patterns in text. The slashes
+# on each side of the expression are pins.
+
+/ruby/ , /[0-9]+
+
+/^{\d{3}-\d{3}-\d{4}/
