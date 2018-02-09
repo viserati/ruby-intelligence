73b87ffc8a43329030d36979468bd2667ec31a8a
Keith Turner <turner@keithturner.com>
2018-02-09 07:58:49 -0700

    Reading code

--- practice.rb
+++ practice.rb
@@ -8,3 +8,38 @@ require 'net/http'
 Net::HTTP.start( 'www.ruby-lang.org', 80 ) do |http|
 print( http.get( '/en/LICENSE.txt' ).body )
 end
+
+=begin
+                              Reading Code
+
+Find some Ruby code to read. You should be reading any Ruby code you can and
+trying to steal ideas that you find. You actually should have enough knowledge 
+to be able to read but maybe not understand what the code does. What this
+lesson teaches is how to apply things you have learned to understand other
+people's code.
+
+First, print out the code you want to understand. Yes, print it out, because
+your eyes and brain are more used to reading paper than computer screens. Make sure you print a few pages at a time.
+
+Second, go through your printout and take notes on the following:
+
+1. Functions and what they do.
+2. Where each variable is first given a value.
+3. Any variables with the same names in different parts of the program. These may be trouble later.
+4. Any if-statements without else clauses. Are they right?
+5. Any while-loops that might not end.
+6. Any parts of code that you can't understand for whatever reason.
+
+Third, once you have all of this marked up, try to explain it to yourself by
+writing comments as you go. Explain the functions, how they are used, what
+variables are involved and anything you can to figure this code out.
+
+Lastly, on all of the difficult parts, trace the values of each variable line by
+line, function by function. In fact, do another printout, and write in the
+margin the value of each variable that you need to "trace."
+
+Once you have a good idea of what the code does, go back to the computer and
+read it again to see if you find new things. Keep finding more code and doing
+this until you do not need the printouts anymore.
+
+=end
