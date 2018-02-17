e73defac91fa1d9d337e4c73df450ae6a2f966a1
Keith Turner <turner@keithturner.com>
2018-02-17 09:55:31 -0700

    Add exercise.

--- sandbox.rb
+++ sandbox.rb
@@ -1,3 +1,19 @@
+=begin
+
 7.times do |count|
   puts "Checking out this block behavior #{count + 1}"
+end
+
+=end
+
+=begin
+Use the each method of Array to iterate over [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+and print out each value.
+=end
+
+artist = ["Picasso", "Pollack", "Miro"]
+
+# multi-line version
+artist.each do |person|
+  puts "#{person} is a painter."
 end
