9cc6fca35bd3d010d1c6610325b070b7d3e6a772
Keith Turner <turner@keithturner.com>
2018-02-18 19:36:06 -0700

    Update strings.

--- strings.rb
+++ strings.rb
@@ -1,9 +1,19 @@
 # Strings are any sort of characters surrounded by quotes, 'single' or "double", quotes.
 
+=begin
+=end
+
 "sealab", '2021', 'The quick brown fox'
 
 # Ex. 1: with double quotes
 " The man said, 'Hi there!'"
 
-#Ex. 2: with single quotes and escaping
+# Ex. 2: with single quotes and escaping
 'The man said, \Hi there!\ '
+
+# Ex. 3: double quotes allow for string interpolation #{ }.
+
+
+number = 10
+
+puts "My favorite number is #{number}."
