a8f041dca55c5a31c021f65e68383dcc2f131dfd
Keith Turner <turner@keithturner.com>
2018-02-18 19:29:02 -0700

    Update strings.

--- strings.rb
+++ strings.rb
@@ -1,3 +1,9 @@
 # Strings are any sort of characters surrounded by quotes, 'single' or "double", quotes.
 
 "sealab", '2021', 'The quick brown fox'
+
+# Ex. 1: with double quotes
+" The man said, 'Hi there!'"
+
+#Ex. 2: with single quotes and escaping
+'The man said, \Hi there!\ '
