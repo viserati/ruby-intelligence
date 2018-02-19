d55c96be4c20ee666b4b4bb1ce73fecb22010027
Keith Turner <turner@keithturner.com>
2018-02-18 19:45:28 -0700

    Update numbers.

--- numbers.rb
+++ numbers.rb
@@ -1,7 +1,7 @@
 # The most basic kind of numbers are integers.
 
-1, 23, -10000
+1, 23, -10000, 4349097597
 
 # Decimal numbers are called floats.
 
-3.14, -808.08, 12.043
+3.14, -808.08, 12.043, 98.6, 2341.6754
