3e69cfa88ac55c6f68c1ed0c4a541b84a9a90170
Keith Turner <turner@keithturner.com>
2018-02-19 07:42:34 -0700

    Add operations.

---
+++ operations.rb
@@ -0,0 +1,34 @@
+# Operations or Operators
+
+1 + 1 # addition
+2 - 2 # subtraction
+3 * 3 # multiplication
+4 / 4 # division
+16 % 5 # modulo or modulus solves for what the remainder is.
+15.0 / 4 # division using floats
+
+=begin
+The equality operator is used when to want to check if two objects are the same.
+It compares the object on the left to the object on the right and returns true
+or false (boolean values).
+=end
+
+4 == 4
+
+4 == 5
+
+'water' == 'water'
+
+'water' == 'sand'
+
+# String concatenation is combining strigs together.
+
+'Pauly' + 'Walnuts'
+
+# Type conversion is converting from one data type to another.
+
+'4'.to_i # converts the string '4' to an integer of 4.
+
+'4'.to_f # converts the string '4' to an fixnum of 4.0
+
+5.to_s # converts the integer 5 to a string of '5'.
