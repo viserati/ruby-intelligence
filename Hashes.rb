e86228f48f6815f54a74049e77bb3ddbdaee2cdd
Keith Turner <turner@keithturner.com>
2018-02-20 09:03:33 -0700

    Update hashes.

--- Hashes.rb
+++ Hashes.rb
@@ -4,11 +4,14 @@ with their definitions. Ruby does so with a hash rocket =>.
 Unlike arrays, the items in a hash are not kept in a specific order.
 =end
 
-{key => value} # Think key/value pair.
+{:key => value} # Think key/value pair.
 
-{sw => 'southwest_airlines', aa => 'american_airlines', ua => 'united_airlines'}
+{:sw => 'southwest_airlines', aa => 'american_airlines', ua => 'united_airlines'}
 
 { 'Parrot' => 'Bird',
   'Polly' => 'Parrot',
   'Bird' => 'fly'
 }
+
+# The most important thing to remember about hashes is that you use keys to
+# set or retrieve values.
