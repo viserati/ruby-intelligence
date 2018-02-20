cf00d0a7eda07ff9bf15e423a4b595150f758229
Keith Turner <turner@keithturner.com>
2018-02-20 12:40:49 -0700

    Update hashes amd misc.

--- Hashes.rb
+++ Hashes.rb
@@ -15,3 +15,17 @@ Unlike arrays, the items in a hash are not kept in a specific order.
 
 # The most important thing to remember about hashes is that you use keys to
 # set or retrieve values.
+
+# **** !!!! THE NEW RUBY SYNTAX OR HASHES !!!! ****
+
+movies = { jaws: 1975,
+           anchorman: 2004,
+           man_of_steel: 2013,
+           a_beautiful_mind: 2001,
+           the_evil_dead: 1981 }
+
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
--- misc.rb
+++ misc.rb
@@ -1,8 +1,13 @@
-bill = "male"
-mary = "female"
-tom = "male"
-tina = "female"
+movies = {
+Michael_Clayton: 2007,
+Fight_Club: 1999,
+Barton_Fink: 1991,
+Bourne_Identity: 2002,
+Stealing_Beauty: 1996
+}
 
-puts bill, mary, tom, tina
-
-puts tina == mary
+puts movies[:Michael_Clayton]
+puts movies[:Fight_Club]
+puts movies[:Barton_Fink]
+puts movies[:Bourne_Identity]
+puts movies[:Stealing_Beauty]
