32567b562a5616d87153da82adc54a4956801f38
Keith Turner <turner@keithturner.com>
2018-02-20 21:48:49 -0700

    Update misc.

--- misc.rb
+++ misc.rb
@@ -1,3 +1,8 @@
-name = gets.chomp
+puts "What's your first name?"
+f_name = gets.chomp
 
-puts name + ' is a genius!'
+puts "What's your last name?"
+l_name = gets.chomp
+
+
+puts f_name ' ' + l_name
